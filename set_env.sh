da5375be7a617bd826a66d44c6f9c90291317023
bitresearch2006 <bitresearch2006@gmail.com>
2026-06-17 19:01:50 +0530

    Update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -2,7 +2,10 @@
 # source set_env.sh
 
 export GRADLE_ARGS="--info"
+export EXECUTION_TIMEOUT="1800"
 
 echo "Environment variables set:"
 echo "  GRADLE_ARGS=$GRADLE_ARGS"
+echo "  EXECUTION_TIMEOUT=$EXECUTION_TIMEOUT"
+
 
